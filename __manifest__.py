1b44f1c966ed25833850c7c5c1db6a60264d4170
MAJiiN999 <mcrespoo999@gmail.com>
2025-01-22 17:19:31 +0100

    kokito

--- __manifest__.py
+++ __manifest__.py
@@ -8,7 +8,7 @@
 Long description of module's purpose
     """,
 
-    'author': "My Company",
+    'author': "Jhonny",
     'website': "https://www.yourcompany.com",
 
     # Categories can be used to filter modules in modules listing
